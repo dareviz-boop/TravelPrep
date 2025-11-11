255f2840c3abfa93703a4b7f41214f5dafe57422
Nancya <dareviz.contact@gmail.com>
2025-11-11 19:58:43 +0800

    Update toast.tsx

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -28,7 +28,7 @@ const toastVariants = cva(
     variants: {
       variant: {
         default: "border bg-background text-foreground",
-        destructive: "destructive group border-destructive bg-destructive text-destructive-foreground",
+        destructive: "destructive group border-destructive/50 bg-destructive-background text-destructive",
       },
     },
     defaultVariants: {
